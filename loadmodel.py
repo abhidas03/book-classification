80db72ad5996585347e02972a7c075d40d11706d
Abhijit Das <adas3@strelka.swarthmore.edu>
2023-07-17 15:54:04 -0400

    kk

--- loadmodel.py
+++ loadmodel.py
@@ -3,7 +3,7 @@ import keras.models
 import tensorflow as tf
 import numpy as np
 from collections import defaultdict
-
+import math
 num_to_class = {0: 'Arts & Photography', 1: 'Biographies & Memoirs', 2:'Business & Money', 3: 'Calendars', 
                 4: 'Children\'s Books',	5: 'Comics & Graphic Novels', 6:'Computers & Technology',  7: 'Cookbooks, Food & Wine',
                 8: 'Crafts, Hobbies & Home', 9: 'Christian Books & Bibles', 10: 'Engineering & Transportation',  11: 'Health, Fitness & Dieting',
@@ -19,7 +19,7 @@ num_to_class = {0: 'Arts & Photography', 1: 'Biographies & Memoirs', 2:'Business
 amountCorrectForEachCategory = defaultdict(list)
 
 
-model = keras.models.load_model("./saved_models/model_secondsave/")
+model = keras.models.load_model("./saved_models/model_asave/")
 
 def convertImage(filePath):
     image = tf.keras.utils.load_img(filePath)
@@ -34,36 +34,43 @@ def convertImage(filePath):
 # if wrong add one to second number
 f = open("bookcover30-labels-test.txt")
 from keras.applications.vgg16 import preprocess_input
-for line in f:
+
+
+bigSumOfError = 0
+count = 0
+for i in range(200):
+    line = f.readline()
+    count += 1
     image, category = line.split()
     temp = image
-    #----
-    # if (int(category) != 4):
-    #     continue
-    #----
+
     image = np.array(convertImage('input/224x224/{0}'.format(image)))
+    image = image.astype(float)
     image = image.reshape(-1, 224, 224, 3)
-    image = image.astype('float32')
-    # preprocess_input(image)
-    image = image/255
+    preprocess_input(image)
+    # image = image/255
 
     y_prob = model.predict(image)
     y_classes = y_prob.argmax(axis=-1)[0]
     category = int(category)
-    if (y_classes == category):
-        amountCorrectForEachCategory.setdefault(category, [0, 0])[0] += 1
-    else:
-        amountCorrectForEachCategory.setdefault(category, [0, 0])[1] += 1
-    #----
-    # print('{0} Actual:{1} Prediction:{2}'.format(temp, num_to_class[category], num_to_class[y_classes]))
-    #----
+
+    patError= y_prob[0]
+    print(y_prob)
+    patError[category] = patError[category]-1
+    sumTotal = 0
+    for i in range(len(patError)):
+         sumTotal += patError[i]**2
+    
+    bigSumOfError += math.sqrt(sumTotal) 
+
+    # if (y_classes == category):
+    #     amountCorrectForEachCategory.setdefault(category, [0, 0])[0] += 1
+    # else:
+    #     amountCorrectForEachCategory.setdefault(category, [0, 0])[1] += 1
+
+print(bigSumOfError / count)
 
 for key in amountCorrectForEachCategory:
     key = int(key)
     print('Amount correct for {}: {}/{}'.format(num_to_class[key], amountCorrectForEachCategory[key][0], amountCorrectForEachCategory[key][1] + amountCorrectForEachCategory[key][0]))
 
-#<<<
-# y_prob = model.predict(image)
-# y_classes = y_prob.argmax(axis=-1)
-# print(num_to_class[y_classes[0]])
-#<<<
